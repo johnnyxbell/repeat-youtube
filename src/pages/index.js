7287640a315030fd4186697c481ce5f71dfc99c6
johnnyxbell <hi@johnnybell.io>
2020-05-15 16:24:19 -0700

    very basic app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import YouTube from 'react-youtube';
 
 const Index = () => {
-  return <div>Repeat YouTube</div>;
+  const [values, setValues] = useState({});
+
+  const handleInputChange = (e, setValues) => {
+    const {name, value} = e.target;
+    setValues((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const videoId = values && values.video && values.video.split('=')[1];
+
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+      loop: 1,
+    },
+  };
+
+  const _onReady = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.playVideo();
+  };
+
+  return (
+    <div>
+      Repeat YouTube
+      <input
+        placeholder='Put in your youtube video link'
+        value={values.video ? values.video : ''}
+        onChange={(e) => handleInputChange(e, setValues)}
+        name='video'
+        id='video'
+      />
+      {videoId && <YouTube videoId={videoId} opts={opts} onReady={_onReady} />}
+    </div>
+  );
 };
 export default Index;
